dc1526cf589a64882770c1b1f2cc28eca3df0d17
Nechaev101 <nechaev.kv@r1pro.ru>
2021-10-31 23:15:16 +0500

    Laravel - 1 задание. request url fix

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::   get('/products',      [ProductController::class, 'GetProducts']);
-Route::   get('/product/{id}',  [ProductController::class, 'GetProduct']);
+Route::   get('/products/{id}',  [ProductController::class, 'GetProduct']);
 Route::  post('/products',      [ProductController::class, 'AddProduct']);
 Route::   put('/products',      [ProductController::class, 'UpdateProduct']);
 Route::delete('/products/{id}', [ProductController::class, 'DeleteProduct']);
